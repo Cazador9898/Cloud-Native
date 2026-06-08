75b4e6dd04bfd5545b9940e9bb95a3fdf958f4ec
Cazador9898 <alonsoguzmanruz@gmail.com>
2026-06-08 00:04:26 -0400

    Delete Proyecto_Ex1_S3_Guias_y_Transporte/Proyecto_Ex1_S3_Guias_y_Transporte directory

--- Proyecto_Ex1_S3_Guias_y_Transporte/Proyecto_Ex1_S3_Guias_y_Transporte/src/main/java/com/example/proyecto/repository/TransportistaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyecto.repository;
-
-import com.example.proyecto.model.Transportista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportistaRepository extends JpaRepository<Transportista, Long> {
-}
